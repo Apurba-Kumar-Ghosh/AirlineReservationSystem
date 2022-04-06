92285c89fc6687868658fdb4c34412d70ee688a1
Apurba Kr. Ghosh <apurba.kumarghosh.ece22@heritageit.edu.in>
2022-04-06 22:42:41 +0530

    showing password field only to admin users

--- src/app/Components/login-page/login-page.component.ts
+++ src/app/Components/login-page/login-page.component.ts
@@ -10,15 +10,21 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class LoginPageComponent implements OnInit {
   public userName: string;
   public password: string;
+  public isAdmin: boolean;
   constructor(private router: Router, private route: ActivatedRoute) {
     this.userName = '';
     this.password = '';
+    this.isAdmin = false;
   }
   onSubmit(form: NgForm): void {
+    sessionStorage.setItem('UserName', this.userName);
     this.router.navigate(['home'], { relativeTo: this.route });
     console.log(
       'formdata: Name : ' + this.userName + ' password: ' + this.password
     );
+  }
+  showPasswordField() {
+    this.isAdmin = !this.isAdmin;
   }
   ngOnInit(): void {}
 }
